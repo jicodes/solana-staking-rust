b5972a1034206d30eb05d24fff6fa87d1d7c34b2
jicodes <122512755+jicodes@users.noreply.github.com>
2024-09-11 18:45:33 -0700

    feat: add init to create staking token vault account and tests

--- programs/solana-staking-rust/src/lib.rs
+++ programs/solana-staking-rust/src/lib.rs
@@ -1,6 +1,13 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("63uB2o4jHubS5RJUWYt8QwAp74RqzBrt4Eci3rzgL7sC");
+
+pub mod constants {
+    pub const VAULT_SEED: &[u8] = b"vault";
+    pub const STAKE_INFO_SEED: &[u8] = b"stake_info";
+    pub const TOKEN_SEED: &[u8] = b"token";
+}
 
 #[program]
 pub mod solana_staking_rust {
@@ -13,4 +20,22 @@ pub mod solana_staking_rust {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init_if_needed,
+        payer = signer,
+        seeds = [constants::VAULT_SEED],
+        bump,
+        token::mint = mint,
+        token::authority = token_vault_account, // to sign the transfer, authority is the vault account itself
+    )]
+    pub token_vault_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
--- tests/solana-staking-rust.ts
+++ tests/solana-staking-rust.ts
@@ -1,16 +1,56 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaStakingRust } from "../target/types/solana_staking_rust";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { createMint, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
 
 describe("solana-staking-rust", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
+  const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+  const mintKeypair = Keypair.fromSecretKey(
+    new Uint8Array([
+      233, 17, 81, 131, 170, 54, 236, 195, 63, 232, 175, 37, 50, 127, 112, 122,
+      0, 136, 28, 97, 220, 51, 237, 143, 170, 230, 221, 181, 187, 237, 244, 189,
+      25, 69, 97, 249, 57, 196, 2, 65, 198, 8, 64, 121, 55, 14, 22, 131, 71, 19,
+      129, 233, 18, 143, 197, 186, 179, 27, 80, 241, 92, 170, 255, 189,
+    ]),
+  );
 
-  const program = anchor.workspace.SolanaStakingRust as Program<SolanaStakingRust>;
+  const program = anchor.workspace
+    .SolanaStakingRust as Program<SolanaStakingRust>;
+
+  async function createMintToken() {
+    const mint = await createMint(
+      connection,
+      payer.payer,
+      payer.publicKey,
+      payer.publicKey,
+      9,
+      mintKeypair,
+    );
+    console.log("Mint:", mint);
+  }
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    // await createMintToken();
+
+    let [vaultAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("vault")],
+      program.programId,
+    );
+
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        tokenVaultAccount: vaultAccount,
+        signer: payer.publicKey,
+        mint: mintKeypair.publicKey,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
